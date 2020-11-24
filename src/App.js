d7dbc292215fa4a74640d5b5fec79a2dfc23e442
kanderson425 <kyleanderson.dev@gmail.com>
2020-11-24 10:38:35 -0600

    First commit, sets up project.

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Covid-19 Tracker</h1>
+
+      {/* Header */}
+      {/* Title + Select Input dropdown field */}
+
+      {/* Info boxes */}
+      {/* Info boxes */}
+      {/* Info boxes */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
+    </div>
+  );
+}
+
+export default App;
