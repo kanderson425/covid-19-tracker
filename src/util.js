6902338a35c79290d953c4dd3d9c65cbce0f05c3
kanderson425 <kyleanderson.dev@gmail.com>
2020-11-27 13:02:35 -0600

    Adds the styling for the PopUp component.

--- src/util.js
+++ src/util.js
@@ -35,7 +35,22 @@ export const showDataOnMap = (data, casesType = "cases") =>
       }
     >
       <Popup>
-        <h1>IM A POPUP</h1>
+        <div className="info-container">
+          <div
+            className="info-flag"
+            style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+          ></div>
+          <div className="info-name">{country.country}</div>
+          <div className="info-confirmed">
+            Cases: {numeral(country.cases).format("0,0")}
+          </div>
+          <div className="info-reovered">
+            Recovered: {numeral(country.recovered).format("0,0")}
+          </div>
+          <div className="info-deaths">
+            Deaths: {numeral(country.deaths).format("0,0")}
+          </div>
+        </div>
       </Popup>
     </Circle>
   ));
